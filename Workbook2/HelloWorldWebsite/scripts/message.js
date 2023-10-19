5d6a4a7a74c95cc1c7ac69585f1578813dea0dd8
Gregory Testo <gt@gregorytesto.com>
2023-10-19 14:45:18 -0400

    added inputJsExample file

--- Workbook2/HelloWorldWebsite/scripts/message.js
+++ Workbook2/HelloWorldWebsite/scripts/message.js
@@ -1,7 +1,11 @@
 "use strict";
 
 function showMessage(){
-    let messageEl = document.getElementById("message");
-    messageEl.innerHTML = "Hello World";
+    const firstName = "Gregorio";
+    const messageEl = document.getElementById("message");
+    // Hello __, it's nice to meet you!
+    // messageEl.innerHTML = "Hello " + firstName + ", it's nice to meet you!"; // Concatenation
+    messageEl.innerHTML = `Hello ${firstName}, it's nice to meet you!`; // Interpolation
 }
+
 showMessage();
