3ad27aecb328af1ebe18f8b33976fade10d0d936
Gregory Testo <gt@gregorytesto.com>
2023-10-31 15:15:45 -0400

    added cheap candy exercise starter code

---
+++ Workbook4/CodeAlong/cheapCandy.js
@@ -0,0 +1,44 @@
+let products = [
+    { product: "Gummy Worms", price: 5.79 },
+    { product: "Plain M&Ms", price: 2.89 },
+    { product: "Peanut M&Ms", price: 2.89 },
+    { product: "Swedish Fish", price: 3.79 },
+    { product: "Sour Patch Kids", price: 2.99 },
+    { product: "Jelly Beans", price: 4.49 },
+    { product: "Chocolate Truffles", price: 7.99 },
+    { product: "Licorice Twists", price: 1.99 },
+    { product: "Caramel Chews", price: 3.29 },
+    { product: "Cotton Candy", price: 1.49 },
+    { product: "Rock Candy", price: 3.99 },
+    { product: "Jawbreakers", price: 0.99 },
+    { product: "Toffee", price: 4.79 },
+    { product: "Mint Chocolate Chips", price: 2.49 },
+    { product: "Hard Candy Lollipops", price: 1.29 },
+    { product: "Bubblegum Balls", price: 0.79 },
+    { product: "Fruit Gushers", price: 3.49 },
+    { product: "Candy Canes", price: 1.59 },
+    { product: "Caramel Popcorn", price: 4.99 },
+    { product: "Chocolate Covered Raisins", price: 2.69 },
+];
+
+// What candies cost less than $4.00?
+// How many candies in the array have "M&M" in their name?
+// What is the average price of all the candies in the array?
+// Which candy is the most expensive in the array?
+// Can you list all the candies that cost exactly $2.89?
+// How many candies have a price that ends in ".99" (cents)?
+// What is the total cost if you buy one of each candy in the array?
+// Which candy has the longest name?
+// Are there any candies with a price lower than $1.00?
+// Can you find the candy with the shortest name?
+
+// Which candies have a price greater than or equal to $5.00?
+// What is the total number of candies in the array?
+// List the candies in alphabetical order by their names.
+// Which candy has the highest price among those with names containing the letter "i"?
+// How many candies have names consisting of two words?
+// What is the combined price of all candies with "M&M" in their name?
+// Are there any candies with a price between $2.50 and $3.00?
+// Which candy has the lowest price among those with names containing the letter "o"?
+// How many candies have names that are longer than 10 characters?
+// What is the difference in price between the most expensive and least expensive candies in the array?
