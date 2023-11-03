163eb2a55c1c6e6122745614b2c2ea7bc2b483c6
Gregory Testo <gt@gregorytesto.com>
2023-11-03 11:03:32 -0400

    added activities example

---
+++ Workbook4/CodeAlong/truthyFalsyPlayground.js
@@ -0,0 +1,23 @@
+// let word = "";
+
+// if(word){
+//     console.log("True");
+// } else {
+//     console.log("False");
+// }
+
+// let count = 0;
+// count++;
+// if(count){
+//     console.log("True");
+// } else {
+//     console.log("False");
+// }
+
+let colors = ["Green"]
+
+if(colors.length){
+    console.log("True");
+} else {
+    console.log("False")
+}
