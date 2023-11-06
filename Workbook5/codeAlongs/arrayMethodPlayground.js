aefdb4d3cdceb22ef0698ad6f391b40a7fb3441b
Gregory Testo <gt@gregorytesto.com>
2023-11-06 16:05:35 -0500

    added method examples

--- Workbook5/codeAlongs/arrayMethodPlayground.js
+++ Workbook5/codeAlongs/arrayMethodPlayground.js
@@ -13,23 +13,59 @@ let menuItems = [
 //     { id: 3, item: "Salad", category: "Meal", price: 8.29 },
 // ]
 
+
+let itemsMatchingCategory = menuItems.filter(function(item){
+    return item.category === "Drink";
+});
+console.log(itemsMatchingCategory);
+
+// ***********************
+
 // Accumulator Pattern for filtering!
-let category = "Meal";
+// function filterMenuItemsByCategory(items, category) {
 
-// Create Accumulator
-let menuItemsMatchCategory = [];
+//     // Create Accumulator
+//     let menuItemsMatchCategory = [];
 
-// loop
-for(let item of menuItems){
-    if(item.category === category){
-        // Act of Acumulating!
-        menuItemsMatchCategory.push(item);
-    }
-}
+//     // loop
+//     for (let item of items) {
+//         if (item.category === category) {
+//             // Act of Acumulating!
+//             menuItemsMatchCategory.push(item);
+//         }
+//     }
 
-console.log(menuItemsMatchCategory)
+//     // Return accumulator
+//     return menuItemsMatchCategory;
+// }
+
+// let filteredItems = filterMenuItemsByCategory(menuItems, "Drink");
+// console.log(filteredItems);
 
 // **********************
+
+// let menuItems = [
+//     { id: 1, item: "Tacos", category: "Meal", price: 12.29 },
+//     { id: 2, item: "Burger", category: "Meal", price: 7.29 },
+//     { id: 3, item: "Salad", category: "Meal", price: 8.29 },
+//     { id: 4, item: "Ice tea", category: "Drink", price: 2.19 },
+//     { id: 5, item: "Coke", category: "Drink", price: 2.29 },
+// ];
+
+// Callback function
+// function findById(item){
+//     if(item.id = 2){
+//         return true;
+//     }
+// }
+
+// let foundItem = menuItems.find(findById);
+// console.log(foundItem);
+
+// let foundItem = menuItems.find(function(item){
+//     return item.id === 2;
+// });
+// console.log(foundItem);
 
 // Accumulator Pattern for searching!
 
