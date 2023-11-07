cbed6517ac52f900421cb3df20f94ca45574ceb7
Gregory Testo <gt@gregorytesto.com>
2023-11-07 09:45:13 -0500

    added codewars template file

---
+++ Codewars/CodeAlong/template.js
@@ -0,0 +1,33 @@
+// DESCRIPTION:
+
+// Inputs/Outputs:
+// () => 
+// () => 
+// () =>
+
+// Poyla 4 steps of problem solving
+
+// Step 1: Understand the problem
+// Step 2: Devise a plan
+
+// Accumulator Pattern:
+// **
+// Step 1: Create an accumulator variable
+// Step 2: Loop
+// Step 3 (inside loop): Accumulate
+// Step 4: return accumulator
+// **
+
+// Step 3: Code
+// Step 4: Look back
+
+function algoFunc(/* Inputs */) {
+
+    return /* Outputs */;
+}
+
+let result1 = algoFunc(/* inputs */); //
+let result2 = algoFunc(/* inputs */); //
+let result3 = algoFunc(/* inputs */); //
+
+console.log(result1, result2, result3);
