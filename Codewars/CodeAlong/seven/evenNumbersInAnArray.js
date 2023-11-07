9843770fa84a3d1c68a964da704f6ffdab64771c
Gregory Testo <gt@gregorytesto.com>
2023-11-07 08:54:39 -0500

    added first codewars in class warmup

---
+++ Codewars/CodeAlong/seven/evenNumbersInAnArray.js
@@ -0,0 +1,28 @@
+// DESCRIPTION:
+// Given an array of numbers, return a new array of length number containing the last even numbers from the original array (in the same order). The original array will be not empty and will contain at least "number" even numbers.
+
+// For example:
+
+// ([1, 2, 3, 4, 5, 6, 7, 8, 9], 3) => [4, 6, 8]
+// ([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2) => [-8, 26]
+// ([6, -25, 3, 7, 5, 5, 7, -3, 23], 1) => [6]
+
+// Poyla 4 steps of problem solving
+
+// Step 1: Understand the problem
+// Step 2: Devise a plan
+
+// Accumulator Pattern:
+    // **
+    // Step 1: Create an accumulator variable
+    // Step 2: Loop
+        // Step 3 (inside loop): Accumulate
+    // Step 4: return accumulator
+    // **
+
+// Step 3: Code
+// Step 4: Look back
+
+function evenNumbers(array, number) {
+    
+}
