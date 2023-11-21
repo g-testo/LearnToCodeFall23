401bb4b9fcc610ddb5fd4d4dbe4f6668e6ad03e9
Gregory Testo <gt@gregorytesto.com>
2023-11-21 09:22:07 -0500

    reorganized the review files

---
+++ Workbook6/CodeAlongs/review1.js
@@ -0,0 +1,18 @@
+// Most common helper methods
+
+// ****************************************
+
+// Arrays
+
+// Docs: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+// Methods: 
+
+let numArr = [9, 8, 7, 6, 5, 4, 3, 2];
+let strArr = ["Lions", "Tigers", "And", "Bears"];
+
+// .forEach
+// What are the inputs? : Array
+// What are the outputs? (What does it return?) : undefined
+// Does it change the original? (Is it destructive?)
+
