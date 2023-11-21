40d810bd362ffd79a3759e8b645f65af3375661a
Gregory Testo <gt@gregorytesto.com>
2023-11-21 08:54:04 -0500

    added workbook6

---
+++ Workbook6/CodeAlongs/review.js
@@ -0,0 +1,74 @@
+// Most common helper methods
+
+// ****************************************
+
+// Arrays
+
+// Docs: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+
+// Methods: 
+
+// .map
+
+
+// ****************************************
+
+
+// Strings
+
+// Docs: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+
+// ****************************************
+
+// Objects
+
+// Docs: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object
+
+// ****************************************
+
+// Numbers
+
+// Docs: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+// Docs: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number
+
+// ****************************************
+
+
+
+// .forEach
+
+// .split
+
+// .join
+
+// .substring
+
+// .slice
+
+// .find
+
+// .filter
+
+// .reduce
+
+// .sort
+
+// .push
+
+// .shift
+
+// .unshift
+
+// .pop
+
+// .indexOf
+
+// .toString
+
+// .firstIndexOf
+
+// .reverse
+
+// parseInt, .floor, .ceil, .round
+
