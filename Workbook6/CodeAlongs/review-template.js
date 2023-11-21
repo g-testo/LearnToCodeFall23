b9f64cc65e695908c38a33ba59042d63acff65ac
Gregory Testo <gt@gregorytesto.com>
2023-11-21 10:13:10 -0500

    added more formatting to the template

--- Workbook6/CodeAlongs/review-template.js
+++ Workbook6/CodeAlongs/review-template.js
@@ -10,36 +10,170 @@
 // Methods: 
 
 // .forEach
+    // Method Docs: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+
+    // What are the inputs?
+        // Acts upon:
+        // Arguments:
+
+    // What are the outputs? (What does it return?):
+
+    // Does it change the original? (Is it destructive?):
 
 // .map
+    // Method Docs:
+
+    // What are the inputs?
+        // Acts upon:
+        // Arguments:
+
+    // What are the outputs? (What does it return?):
+
+    // Does it change the original? (Is it destructive?):
 
 // .find
+    // Method Docs:
+
+    // What are the inputs?
+        // Acts upon:
+        // Arguments:
+
+    // What are the outputs? (What does it return?):
+
+    // Does it change the original? (Is it destructive?):
 
 // .filter
+    // Method Docs:
 
-// .push
+    // What are the inputs?
+        // Acts upon:
+        // Arguments:
 
-// .shift
+    // What are the outputs? (What does it return?):
 
-// .unshift
+    // Does it change the original? (Is it destructive?):
 
-// .pop
+// Add/Remove
+    // .push
+        // Method Docs:
+
+        // What are the inputs?
+            // Acts upon:
+            // Arguments:
+
+        // What are the outputs? (What does it return?):
+
+        // Does it change the original? (Is it destructive?):
+
+    // .shift
+        // Method Docs:
+        // What are the inputs?
+            // Acts upon:
+            // Arguments:
+
+        // What are the outputs? (What does it return?):
+
+        // Does it change the original? (Is it destructive?):
+
+    // .unshift
+        // Method Docs:
+        // What are the inputs?
+            // Acts upon:
+            // Arguments:
+
+        // What are the outputs? (What does it return?):
+
+        // Does it change the original? (Is it destructive?):
+
+    // .pop
+        // Method Docs:
+        // What are the inputs?
+            // Acts upon:
+            // Arguments:
+
+        // What are the outputs? (What does it return?):
+
+        // Does it change the original? (Is it destructive?):
 
 // .join
+    // Method Docs:
+    // What are the inputs?
+        // Acts upon:
+        // Arguments:
+
+    // What are the outputs? (What does it return?):
+
+    // Does it change the original? (Is it destructive?):
 
 // .slice
+    // Method Docs:
+    // What are the inputs?
+        // Acts upon:
+        // Arguments:
+
+    // What are the outputs? (What does it return?):
+
+    // Does it change the original? (Is it destructive?):
 
 // .reduce
+    // Method Docs:
+    // What are the inputs?
+        // Acts upon:
+        // Arguments:
+
+    // What are the outputs? (What does it return?):
+
+    // Does it change the original? (Is it destructive?):
 
 // .sort
+    // Method Docs:
+    // What are the inputs?
+        // Acts upon:
+        // Arguments:
+
+    // What are the outputs? (What does it return?):
+
+    // Does it change the original? (Is it destructive?):
 
 // .includes
+    // Method Docs:
+    // What are the inputs?
+        // Acts upon:
+        // Arguments:
+
+    // What are the outputs? (What does it return?):
+
+    // Does it change the original? (Is it destructive?):
 
 // .reverse
+    // Method Docs:
+    // What are the inputs?
+        // Acts upon:
+        // Arguments:
+
+    // What are the outputs? (What does it return?):
+
+    // Does it change the original? (Is it destructive?):
 
 // .indexOf
+    // Method Docs:
+    // What are the inputs?
+        // Acts upon:
+        // Arguments:
+
+    // What are the outputs? (What does it return?):
+
+    // Does it change the original? (Is it destructive?):
 
 // .firstIndexOf
+    // Method Docs:
+    // What are the inputs?
+        // Acts upon:
+        // Arguments:
+
+    // What are the outputs? (What does it return?):
+
+    // Does it change the original? (Is it destructive?):
 
 // ****************************************
 
@@ -50,28 +184,124 @@
 // Methods:
 
 // .split
+    // Method Docs:
+    // What are the inputs?
+        // Acts upon:
+        // Arguments:
+
+    // What are the outputs? (What does it return?):
+
+    // Does it change the original? (Is it destructive?):
 
 // .includes
+    // Method Docs:
+    // What are the inputs?
+        // Acts upon:
+        // Arguments:
+
+    // What are the outputs? (What does it return?):
+
+    // Does it change the original? (Is it destructive?):
 
 // .substring
+    // Method Docs:
+    // What are the inputs?
+        // Acts upon:
+        // Arguments:
+
+    // What are the outputs? (What does it return?):
+
+    // Does it change the original? (Is it destructive?):
 
 // .slice
+    // Method Docs:
+    // What are the inputs?
+        // Acts upon:
+        // Arguments:
+
+    // What are the outputs? (What does it return?):
+
+    // Does it change the original? (Is it destructive?):
 
 // .charAt
+    // Method Docs:
+    // What are the inputs?
+        // Acts upon:
+        // Arguments:
+
+    // What are the outputs? (What does it return?):
+
+    // Does it change the original? (Is it destructive?):
 
 // .replace
+    // Method Docs:
+    // What are the inputs?
+        // Acts upon:
+        // Arguments:
+
+    // What are the outputs? (What does it return?):
+
+    // Does it change the original? (Is it destructive?):
 
 // .replaceAll
+    // Method Docs:
+    // What are the inputs?
+        // Acts upon:
+        // Arguments:
+
+    // What are the outputs? (What does it return?):
+
+    // Does it change the original? (Is it destructive?):
 
 // .match
+    // Method Docs:
+    // What are the inputs?
+        // Acts upon:
+        // Arguments:
+
+    // What are the outputs? (What does it return?):
+
+    // Does it change the original? (Is it destructive?):
 
 // .startsWith / .endsWith
+    // Method Docs:
+    // What are the inputs?
+        // Acts upon:
+        // Arguments:
+
+    // What are the outputs? (What does it return?):
+
+    // Does it change the original? (Is it destructive?):
 
 // .repeat
+    // Method Docs:
+    // What are the inputs?
+        // Acts upon:
+        // Arguments:
+
+    // What are the outputs? (What does it return?):
+
+    // Does it change the original? (Is it destructive?):
 
 // .toUpperCase / .toLowerCase
+    // Method Docs:
+    // What are the inputs?
+        // Acts upon:
+        // Arguments:
+
+    // What are the outputs? (What does it return?):
+
+    // Does it change the original? (Is it destructive?):
 
 // .trim
+    // Method Docs:
+    // What are the inputs?
+        // Acts upon:
+        // Arguments:
+
+    // What are the outputs? (What does it return?):
+
+    // Does it change the original? (Is it destructive?):
 
 // ****************************************
 
@@ -80,10 +310,34 @@
 // Docs: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object
 
 // .entries
+    // Method Docs:
+    // What are the inputs?
+        // Acts upon:
+        // Arguments:
+
+    // What are the outputs? (What does it return?):
+
+    // Does it change the original? (Is it destructive?):
 
 // .keys
+    // Method Docs:
+    // What are the inputs?
+        // Acts upon:
+        // Arguments:
+
+    // What are the outputs? (What does it return?):
+
+    // Does it change the original? (Is it destructive?):
 
 // .values
+    // Method Docs:
+    // What are the inputs?
+        // Acts upon:
+        // Arguments:
+
+    // What are the outputs? (What does it return?):
+
+    // Does it change the original? (Is it destructive?):
 
 // ****************************************
 
@@ -93,13 +347,53 @@
 // Docs: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number
 
 // .toString
+    // Method Docs:
+    // What are the inputs?
+        // Acts upon:
+        // Arguments:
+
+    // What are the outputs? (What does it return?):
+
+    // Does it change the original? (Is it destructive?):
 
 // parseInt
+    // Method Docs:
+    // What are the inputs?
+        // Acts upon:
+        // Arguments:
+
+    // What are the outputs? (What does it return?):
+
+    // Does it change the original? (Is it destructive?):
 
 // .floor
+    // Method Docs:
+    // What are the inputs?
+        // Acts upon:
+        // Arguments:
+
+    // What are the outputs? (What does it return?):
+
+    // Does it change the original? (Is it destructive?):
 
 // .ceil
+    // Method Docs:
+    // What are the inputs?
+        // Acts upon:
+        // Arguments:
+
+    // What are the outputs? (What does it return?):
+
+    // Does it change the original? (Is it destructive?):
 
 // .round
+    // Method Docs:
+    // What are the inputs?
+        // Acts upon:
+        // Arguments:
+
+    // What are the outputs? (What does it return?):
+
+    // Does it change the original? (Is it destructive?):
 
 // ****************************************
--- Workbook6/CodeAlongs/review1.js
+++ Workbook6/CodeAlongs/review1.js
@@ -12,7 +12,21 @@ let numArr = [9, 8, 7, 6, 5, 4, 3, 2];
 let strArr = ["Lions", "Tigers", "And", "Bears"];
 
 // .forEach
-// What are the inputs? : Array
-// What are the outputs? (What does it return?) : undefined
-// Does it change the original? (Is it destructive?)
 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+
+// What are the inputs?
+// Acts upon: Array 
+// Arguments: Callback function
+
+// What are the outputs? (What does it return?) : undefined
+
+// Does it change the original? (Is it destructive?) No
+
+numArr.forEach((num)=>{
+    console.log(num);
+})
+
+strArr.forEach((str)=>{
+    console.log(str);
+})
