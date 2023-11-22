88ab4684f5c65c638bf23da75d1570464a353c85
Gregory Testo <gt@gregorytesto.com>
2023-11-22 09:14:31 -0500

    added codewars starter code

---
+++ Workbook6/CodeWarProblems/Stop_gninnipS_My_sdroW.js
@@ -0,0 +1,27 @@
+function spinWords(string){
+    // "Hey fellow warriors" => "Hey wollef sroirraw"
+    // _Split_ string into an array of words
+    // Iterate _map_ through the array of words
+        // Ask if the _length_ of the word is greater than 5 "Hey"
+            // (Reverse the word)
+                // _Split_ the word into an array of chars
+                // Use _reverse_ to reverse the array
+                // _Join_ the string together
+                // return new string
+    // _Join_ the array of words together again
+    // Return the new array
+}
+
+// let result1 = spinWords("Welcome");//, "emocleW"
+// console.log(result1);
+
+// let result2 = spinWords("Hey fellow warriors"); // , "Hey wollef sroirraw"
+// console.log(result2);
+
+// let result3 = spinWords("Just kidding there is still one more"); //, "Just gniddik ereht is llits one more"
+// console.log(result3);
+
+
+// let song = "This is the song that never ends...";
+
+// console.log(song.split(""));
