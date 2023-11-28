13de03d4074932beec904b54f5029e0793a2222c
Gregory Testo <gt@gregorytesto.com>
2023-11-28 11:04:02 -0500

    Added random fox example

---
+++ Workbook7/CodeAlongs/workingWithFetch/jsonPlaceholder/notes.js
@@ -0,0 +1,10 @@
+// let response = fetch("https://jsonplaceholder.typicode.com/photos", {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({
+//         name: "Lions",
+//         url: "https://th-thumbnailer.cdn-si-edu.com/kCThhBXn0aeiX7i-J5tIEDHhnFw=/1000x750/filters:no_upscale()/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer/two-male-lions-Kenya-631.jpg",
+//     }),
+// });
