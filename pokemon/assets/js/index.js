e31853b9af35ffe9f88930b0cdc2937893f7adc9
Gregory Testo <gt@gregorytesto.com>
2023-11-30 14:39:11 -0500

    added javascript

--- pokemon/assets/js/index.js
+++ pokemon/assets/js/index.js
@@ -1,3 +1,33 @@
-window.onload =()=>{
+window.onload = () => {
+    let listEl = document.getElementById("list");
+    // Get all pokemon
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=6")
+        .then((res) => res.json())
+        .then((data) => {
+            let pokeList = data.results;
+            for (let pokemon of pokeList) {
+                // Get details about each pokemon
+                fetch(pokemon.url)
+                    .then((res) => res.json())
+                    .then((pokeDetails) => {
+                        let typeStrArr = pokeDetails.types.map(item=>item.type.name);
+                        let cardDiv = document.createElement("div");
+                        cardDiv.className = "card";
 
-}
+                        cardDiv.innerHTML = `<div
+                                class="thumbnail"
+                                style="
+                                    background-image: url(${pokeDetails.sprites.other["official-artwork"].front_default});
+                                "
+                            ></div>
+                            <div class="details">
+                                <div>Name: ${pokeDetails.name}</div>
+                                <div>Type: ${typeStrArr.join(", ")}</div>
+                                <div>Height: ${pokeDetails.height} meter(s)</div>
+                                <div>Weight: ${pokeDetails.weight} lb(s)</div>
+                            </div>`;
+                            listEl.appendChild(cardDiv);
+                    });
+            }
+        });
+};
