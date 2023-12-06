3422575ec4939955724de89564db55d322852edd
Gregory Testo <gt@gregorytesto.com>
2023-12-06 14:28:34 -0500

    added js for handling search

--- pokemonPlayground/index.js
+++ pokemonPlayground/index.js
@@ -1,9 +1,22 @@
 window.onload = async () => {
     let pokemonContainer = document.getElementById("pokemon-container");
+    let searchEl = document.getElementById("search");
 
-    fetch("https://pokemon-backend-dfea.onrender.com/api/pokemon?limit=600")
+    searchEl.oninput = () => {
+        console.log("Trigger");
+        pokemonContainer.innerHTML = "";
+        let convertedPokemonList = JSON.parse(sessionStorage.pokemonList);
+        convertedPokemonList.forEach((pokemon)=>{
+            
+        })
+    };
+
+    fetch("https://pokemon-backend-dfea.onrender.com/api/pokemon?limit=10")
         .then((res) => res.json())
         .then((data) => {
+            sessionStorage.pokemonList = JSON.stringify(data);
+
+
             data.forEach((pokemon) => {
                 let pokemonCard = document.createElement("div");
                 pokemonCard.classList.add("card");
@@ -15,6 +28,8 @@ window.onload = async () => {
 
                 pokemonContainer.appendChild(pokemonCard);
             });
+
+            
         })
         .catch((err) => {
             console.log(err);
