11929e487c7b538c79354f87b301f665f2674ad3
Gregory Testo <gt@gregorytesto.com>
2023-12-06 14:52:16 -0500

    added utility function to load pokemon cards

--- pokemonPlayground/index.js
+++ pokemonPlayground/index.js
@@ -3,35 +3,34 @@ window.onload = async () => {
     let searchEl = document.getElementById("search");
 
     searchEl.oninput = () => {
-        console.log("Trigger");
         pokemonContainer.innerHTML = "";
         let convertedPokemonList = JSON.parse(sessionStorage.pokemonList);
-        convertedPokemonList.forEach((pokemon)=>{
-            
-        })
+
+        loadPokemonCards(convertedPokemonList);
     };
 
     fetch("https://pokemon-backend-dfea.onrender.com/api/pokemon?limit=10")
         .then((res) => res.json())
-        .then((data) => {
-            sessionStorage.pokemonList = JSON.stringify(data);
+        .then((allPokemon) => {
+            sessionStorage.pokemonList = JSON.stringify(allPokemon);
 
-
-            data.forEach((pokemon) => {
-                let pokemonCard = document.createElement("div");
-                pokemonCard.classList.add("card");
-
-                pokemonCard.innerHTML = `
-                    <img src=${pokemon.official_artwork_default} alt=${pokemon.name} />
-                    <p>${pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}</p>
-                `;
-
-                pokemonContainer.appendChild(pokemonCard);
-            });
-
-            
+            loadPokemonCards(allPokemon);
         })
         .catch((err) => {
             console.log(err);
         });
+
+    let loadPokemonCards = (arrayOfPokemon) => {
+        arrayOfPokemon.forEach((pokemon) => {
+            let pokemonCard = document.createElement("div");
+            pokemonCard.classList.add("card");
+
+            pokemonCard.innerHTML = `
+                    <img src=${pokemon.official_artwork_default} alt=${pokemon.name} />
+                    <p>${pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}</p>
+                `;
+
+            pokemonContainer.appendChild(pokemonCard);
+        });
+    };
 };
