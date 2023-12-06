0b141f75b3ffa195516ffdbb7cbcda470a108528
Gregory Testo <gt@gregorytesto.com>
2023-12-06 13:53:38 -0500

    added form to search, sort and filter

--- pokemonPlayground/index.js
+++ pokemonPlayground/index.js
@@ -1,17 +1,22 @@
-
-window.onload= async()=>{
+window.onload = async () => {
     let pokemonContainer = document.getElementById("pokemon-container");
 
-    fetch("https://pokemon-backend-dfea.onrender.com/api/pokemon?limit=20")
-        .then((res)=>res.json())
-        .then((data)=>{
-            data.forEach((pokemon)=>{
+    fetch("https://pokemon-backend-dfea.onrender.com/api/pokemon?limit=600")
+        .then((res) => res.json())
+        .then((data) => {
+            data.forEach((pokemon) => {
                 let pokemonCard = document.createElement("div");
-                pokemonCard.innerHTML = `<img src=${pokemon.official_artwork_default} alt=${pokemon.name} />`;
-                pokemonContainer.appendChild(pokemonCard);
-            })
-        }).catch((err)=>{
-            console.log(err);
-        })
+                pokemonCard.classList.add("card");
 
-}
+                pokemonCard.innerHTML = `
+                    <img src=${pokemon.official_artwork_default} alt=${pokemon.name} />
+                    <p>${pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}</p>
+                `;
+
+                pokemonContainer.appendChild(pokemonCard);
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+};
