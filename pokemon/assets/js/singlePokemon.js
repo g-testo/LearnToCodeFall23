d66c47832552faf7a5949a857757946cd41115a7
Gregory Testo <gt@gregorytesto.com>
2023-12-11 14:22:10 -0500

    added psuedocode to single pokemon page

--- pokemon/assets/js/singlePokemon.js
+++ pokemon/assets/js/singlePokemon.js
@@ -1,6 +1,15 @@
-window.onload = ()=>{
-    console.log(location);
-    // "?id=10&name=bulbasaur"
-    // // 
-    // "bulbasaur"
-}
+window.onload = () => {
+    // Grab pokemon
+    //      Part 1
+    //          URLSearchParams: to get the name of pokemon from URL
+    //      Part 2
+    //          Fetch using the name of the pokemon from part 1 (console.log to confirm this works)
+    // Adding pokemon to the page
+    //      Create placement for pokemon in singlePokemon.html (HTML)
+    //      Get the placement El using getElementById
+
+    //      Create a pokemon El using data from above (Jonathan Style)
+    //      pokemonEl.innerHTML = `<div>${pokemon.name}</div>`
+
+    //      Replacement placement el
+};
